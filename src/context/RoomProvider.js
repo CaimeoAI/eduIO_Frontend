e5c29db5061485979f478bb4243ef3102163422a
Judielle Villa <judiellezinke@yahoo.de>
2023-01-24 12:30:34 +0100

    deployment update v0.1

--- src/context/RoomProvider.js
+++ src/context/RoomProvider.js
@@ -8,7 +8,7 @@ export const useRooms = () => {
      return useContext(RoomContext)
 }
 
-const socket = io.connect(process.env.REACT_BE_URL)
+const socket = io.connect('http://localhost:3001')
 
 export function RoomsProvider({ children }) {
 
